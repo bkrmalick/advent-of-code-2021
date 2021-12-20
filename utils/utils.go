ebf010056ffecf8e64c8e0c6571b7d16fa3d8a84
bkrmalick <bakar@nplan.io>
2021-12-20 03:05:32 +0000

    utils

--- utils/utils.go
+++ utils/utils.go
@@ -10,9 +10,22 @@ import (
 )
 
 
+type Stack []interface{}
+
+func (s *Stack) Push(item interface{}){
+	*s = append(*s, item)
+}
+func (s *Stack) Pop() interface{}{
+	val := (*s)[len(*s)-1]
+	*s = (*s)[:len(*s)-1]
+	return val
+}
+func (s *Stack) IsEmpty() bool{
+	return len(*s) == 0
+}
 
 func SetBasePathToCurrentDir(){
-	_, filename, _, _ := runtime.Caller(1) // skip one stack to ensure we get dir of the calling file and not the utils
+	_, filename, _, _ := runtime.Caller(1) // skip one function in the stack to ensure we get dir of the calling file and not the utils
 	err := os.Chdir(path.Dir(filename))
 	HandleError(err, "updating current working dir")
 }
@@ -79,6 +92,16 @@ func InList(x interface{}, ls interface{}) bool {
 		}
 	}
 	return false
+}
+
+func IndexOf(x interface{}, ls interface{}) int {
+	list := reflect.ValueOf(ls)
+	for i:=0;i<list.Len();i++{
+		if list.Index(i).Interface()==x{
+			return i
+		}
+	}
+	return -1
 }
 
 // StringIntersect intersection using hash map which allows it to operate in O(n) time
